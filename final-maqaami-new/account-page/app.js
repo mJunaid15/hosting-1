438669df0faacd837c94c54a5079d6e5cc2b42ff
Hamza Ilyas <hamza24061@gmail.com>
2021-07-08 12:31:58 +0500

    junaid

--- final-maqaami-new/account-page/app.js
+++
@@ -1,7 +0,0 @@
-function toggle() {
-  let blur = document.getElementById("blur");
-  blur.classList.toggle("active");
-
-  let popup = document.getElementById("popup");
-  popup.classList.toggle("active");
-}
--- final-maqaami-new/customer-panel/mobile-script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  let navigation = document.querySelector(".main-navigation");
-  navigation.classList.toggle("active-mobile");
-
-  let toggle = document.querySelector(".toggle-mobile");
-  toggle.classList.toggle("active-mobile");
-}
